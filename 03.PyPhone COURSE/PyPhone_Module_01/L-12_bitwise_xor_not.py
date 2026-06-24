af4375094b7e7a2285b6c51594aabbf22211a467
Emperor1456 <196229494+Emperor1456@users.noreply.github.com>
2026-06-24 12:08:36 +0600

    Module 01 Finished

---
+++ 03.PyPhone COURSE/PyPhone_Module_01/L-12_bitwise_xor_not.py
@@ -0,0 +1,15 @@
+# Lesson 12: Bitwise XOR and NOT
+
+a = 5
+b = 3
+
+print("a=", a, " b=", b)
+print("a ^ b = ", a ^ b)
+print("~a    = ", ~a)
+print("~b    = ", ~b)
+
+# Try with a negative number
+neg      = -4
+print("\nneg   = ", neg)
+print("~neg  = ", ~neg)
+
---
+++ 03.PyPhone COURSE/PyPhone_Module_01/L-13_bitwise_shifts.py
@@ -0,0 +1,15 @@
+# Lesson 13: Bitwise shift operators
+
+val = 6
+print("Start: ", val)
+
+# Left Shift:
+print("<< 1 : ", val << 1)
+print("<< 2 : ", val << 2)
+
+# Right Shift:
+val = 16
+print("\nStart: ", val)
+print(">> 1 : ", val >> 1)
+print(">> 2 : ", val >> 2)
+print(">> 3 : ", val >> 3)
